b4b4401e438c02ce681e7e4d7855f2de478199d3
sacrhamza <143112928+sacrhamza@users.noreply.github.com>
2026-01-31 17:04:55 +0000

    update one

---
+++ version.sh
@@ -0,0 +1,23 @@
+
+get_version()
+{
+	if check_update;
+	then
+		echo -e "${BLUE}there is un update you can update one using: one --update-self"
+  else
+	echo -e "${BLUE}oneperson tool: ${GREEN_BOLD}v0.0.5${RESET}"
+	fi
+}
+
+check_update()
+{
+  local current_version
+
+	current_version=$(curl https://raw.githubusercontent.com/sacrhamza/one/main/.version 2> /dev/null)
+	if [[ "$PROGRAM_VERSION" != "$current_version" ]]
+	then
+		true
+	else
+		false
+	fi
+}
